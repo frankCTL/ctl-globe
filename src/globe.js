543d3fe699183335f0413226a41d3507d4acdba0
Frank <108413855+frankCTL@users.noreply.github.com>
2023-01-05 14:13:17 +0000

    Editing dataset

--- src/globe.js
+++ src/globe.js
@@ -8,8 +8,7 @@ export function GlobeComponent() {
 
   React.useEffect(() => {
     fetch(
-    //   "https://raw.githubusercontent.com/vasturiano/react-globe.gl/master/example/datasets/ne_110m_admin_0_countries.geojson"
-      "https://raw.githubusercontent.com/frankCTL/ctl-globe/main/dataset/ne_110m_admin_0_countries.geojson?token=GHSAT0AAAAAAB2CVNU2VMEBW4UJ7DNXLIT4Y5W3B4Q"
+"https://raw.githubusercontent.com/frankCTL/ctl-globe/main/dataset/locations.geojson?token=GHSAT0AAAAAAB2CVNU3O2Y5C2LPWTXXHPPWY5W3IWA"
     )
       .then((res) => res.json())
       .then(setCountries);
